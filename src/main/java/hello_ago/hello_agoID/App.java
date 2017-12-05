9c365d67513711d75e340b29240cf64dd29bab19
Marco Agovino <marco.agovino@students.fhnw.ch>
2017-12-05 12:21:46 +0100

    first commit

---
+++ src/main/java/hello_ago/hello_agoID/App.java
@@ -0,0 +1,13 @@
+package hello_ago.hello_agoID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
